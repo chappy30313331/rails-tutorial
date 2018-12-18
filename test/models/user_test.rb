6add71d650c2d457afc1fc1e039c95bcb5ef57c0
chappy30313331 <46666464+chappy30313331@users.noreply.github.com>
2018-12-18 22:41:08 +0900

    Add email uniqueness validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -37,11 +37,26 @@ class UserTest < ActiveSupport::TestCase
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
+
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[foo@bar..com user@example,com user_at_foo.org user.nem@example. foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalide"
     end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 end
