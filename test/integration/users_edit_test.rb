0dd715f4148e5acb1bfcbfe723ae183c66a3c489
chappy30313331 <46666464+chappy30313331@users.noreply.github.com>
2018-12-26 01:16:21 +0900

    Implement authorization

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,6 +7,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: {user: {name: "",
@@ -17,9 +18,12 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select "div.alert", "The form contains 4 errors."
   end
 
-  test "successfull edit" do
+  test "successfull edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    assert_equal edit_user_url(@user), session[:forwarding_url]
+    log_in_as(@user)
+    assert_nil session[:forwarding_url]
+    assert_redirected_to edit_user_url(@user)
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: {user: {name: name,
